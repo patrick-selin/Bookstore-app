4edce14cfe66520f541549d70326e8227915debb
Patrick Selin <patrick.selin@gmail.com>
2023-10-27 10:27:38 +0300

    MUI components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-
 import AddBook from "./AddBook";
+
+import Button from "@mui/material/Button";
+import AppBar from '@mui/material/AppBar';
+import { Toolbar, Typography } from "@mui/material";
 
 function App() {
   // HOOKS
-  //
+  const [books, setBooks] = useState();
 
   // FUNCTIONS
   //
@@ -15,7 +18,15 @@ function App() {
   return (
     <>
       <div>
-        <h1>Bookstore App</h1>
+        
+        <AppBar position="static">
+         <Toolbar>
+          <Typography variant="h4">
+            Bookstore
+          </Typography>
+         </Toolbar>
+        </AppBar>
+        <Button>hei</Button>
         <AddBook></AddBook>
       </div>
     </>
