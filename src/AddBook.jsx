0cbae1240416a96a66e52555ee484f2fc0b5690b
Patrick Selin <patrick.selin@gmail.com>
2023-10-27 12:45:49 +0300

    AddBook styling and functionality

--- src/AddBook.jsx
+++ src/AddBook.jsx
@@ -1,9 +1,93 @@
-function AddBook() {
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+
+
+function AddBook(props) {
+    const [open, setOpen] = useState(false);
+    const [book, setBook] = useState(
+        { 
+        title: "", 
+        author: "", 
+        year: "", 
+        isbn: "", 
+        price: "", 
+    });
+
+
+    const inputChanged = (event) => {
+        setBook({ ...todo, [event.target.name]: event.target.value });
+      };
+
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const handleSave = () => {
+        props.addNewBook(newBook);
+        handleClose();
+    }
     
     return (
       <>
-        <p>item1</p>
-        <p>item2</p>
+        <Button
+          variant="outlined"
+          style={{
+            marginTop: "2rem",
+            marginBottom: "2rem",
+            width: "10rem",
+          }}
+          onClick={handleOpen}
+        >
+          ADD BOOK
+        </Button>
+   
+      <Dialog open={open}>
+        <DialogTitle>New Book</DialogTitle>
+        <DialogContent>
+          <TextField
+            name="title"
+            value={book.title}
+            onChange={inputChanged}
+            margin="dense"
+            label="Description"
+            fullWidth
+          />
+          <TextField
+            name="author"
+            value={book.author}
+            onChange={inputChanged}
+            margin="dense"
+            label="Date"
+            fullWidth
+          />
+          <TextField
+            name="year"
+            value={book.year}
+            onChange={inputChanged}
+            margin="dense"
+            label="Priority"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button color="primary" onClick={handleSave}>
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
         
       </>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,56 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import AddBook from "./AddBook";
 
-import Button from "@mui/material/Button";
 import AppBar from "@mui/material/AppBar";
 import { Toolbar, Typography } from "@mui/material";
 import { AgGridReact } from "ag-grid-react";
 
 import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-alpine.css"
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
-const DATABASE_URL = "https://bookstore-app-26328-default-rtdb.europe-west1.firebasedatabase.app/books/.json"
+const DATABASE_URL =
+  "https://bookstore-app-26328-default-rtdb.europe-west1.firebasedatabase.app/books/.json";
 
 function App() {
   // HOOKS
   const [books, setBooks] = useState();
 
   const [columnDefs] = useState([
-    {field: "eka sarake"},
-    {field: "toka sarake"},
-    {field: "kolmas sarake"},
+    { field: "eka sarake" },
+    { field: "toka sarake" },
+    { field: "kolmas sarake" },
   ]);
 
   useEffect(() => {
     fetchDatabase();
-  }), [];
-  
+  }),
+    [];
+
   // FUNCTIONS
 
   const fetchDatabase = () => {
     fetch(DATABASE_URL)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Fetch error:', error);
-    });
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // data -> books setBooks()
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  };
+
+
+  // addBook() joka lahetetaan propseina AddBook componentille
+  const addNewBook = (newBook) => {
+    console.log('first')
+    // tallena book to books
+    // ja lisaa se tietokantaan
   }
 
 
@@ -54,9 +64,15 @@ function App() {
             <Typography variant="h4">Bookstore</Typography>
           </Toolbar>
         </AppBar>
-        <Button>hei</Button>
-        <AddBook></AddBook>
-        <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      
+        <AddBook addNewBook={addNewBook}></AddBook>
+        <div
+          className="ag-theme-alpine"
+          style={{
+            height: 400,
+            width: 600,
+          }}
+        >
           <AgGridReact rowData={books} columnDefs={columnDefs}></AgGridReact>
         </div>
       </div>
