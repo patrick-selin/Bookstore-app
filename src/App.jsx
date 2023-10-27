2556f914df64f4338c6a0ff7a90e2f83aac1495e
Patrick Selin <patrick.selin@gmail.com>
2023-10-27 10:45:45 +0300

    Ag Grid added

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,40 @@ import "./App.css";
 import AddBook from "./AddBook";
 
 import Button from "@mui/material/Button";
-import AppBar from '@mui/material/AppBar';
+import AppBar from "@mui/material/AppBar";
 import { Toolbar, Typography } from "@mui/material";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 function App() {
   // HOOKS
   const [books, setBooks] = useState();
 
+  const [columnDefs] = useState([
+    {field: "eka sarake"},
+    {field: "toka sarake"},
+    {field: "kolmas sarake"},
+  ]);
+  
   // FUNCTIONS
-  //
+
 
   // RENDER JSX
-
   return (
     <>
       <div>
-        
         <AppBar position="static">
-         <Toolbar>
-          <Typography variant="h4">
-            Bookstore
-          </Typography>
-         </Toolbar>
+          <Toolbar>
+            <Typography variant="h4">Bookstore</Typography>
+          </Toolbar>
         </AppBar>
         <Button>hei</Button>
         <AddBook></AddBook>
+        <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+          <AgGridReact rowData={books} columnDefs={columnDefs}></AgGridReact>
+        </div>
       </div>
     </>
   );
